378cee2f8f95c11c0699745798bb64e6d9ef22d0
ChanwooBrianKim <151323407+ChanwooBrianKim@users.noreply.github.com>
2024-07-25 22:54:23 +1000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import Home from "./pages/Home"; // Corrected import for default export
+import { Store } from "./pages/Store"
+import { About } from "./pages/About"
+import { Navbar } from "./components/Navbar"
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
+  );
+}
+
+export default App
