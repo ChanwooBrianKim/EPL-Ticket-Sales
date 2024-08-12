1d74ac96782a669f78f17bafde0570b60897cd19
ChanwooBrianKim <151323407+ChanwooBrianKim@users.noreply.github.com>
2024-08-12 23:54:53 +1000

    ES Modulisation

--- models/index.js
+++ models/index.js
@@ -1,13 +1,17 @@
-const fs = require('fs'); // Importing the fs module
-const path = require('path'); // Importing the path module
-const Sequelize = require('sequelize'); // Importing the Sequelize module
-const process = require('process'); // Importing the process module
-const basename = path.basename(__filename); // Getting the base name of the current file
-const env = process.env.NODE_ENV || 'development'; // Getting the current environment
-const config = require(__dirname + '/../config/config.json')[env]; // Getting the configuration for the current environment
-const db = {}; // Creating an empty object to store the models
+import fs from 'fs';
+import path from 'path';
+import Sequelize from 'sequelize';
+import { fileURLToPath } from 'url';
+import process from 'process';
+import configJson from '../config/config.json';
 
-// Check if the configuration uses an environment variable
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = configJson[env];
+const db = {};
+
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
@@ -15,7 +19,6 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-// Read all the files in the current directory
 fs
   .readdirSync(__dirname)
   .filter(file => {
@@ -26,20 +29,18 @@ fs
       file.indexOf('.test.js') === -1
     );
   })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
+  .forEach(async file => {
+    const model = await import(path.join(__dirname, file));
+    db[model.default.name] = model.default(sequelize, Sequelize.DataTypes);
   });
 
-// Associate the models
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
 
-// Add the sequelize instance to the db object
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-module.exports = db;
+export default db;
