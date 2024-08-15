ca7a0b87011e531d2ef185725216807b90aa3be5
ChanwooBrianKim <151323407+ChanwooBrianKim@users.noreply.github.com>
2024-08-15 23:15:09 +1000

    Folder moved into the backend folder

---
+++ backend/migrations/20240810073705-create-order.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      totalAmount: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+      },
+      orderStatus: {
+        type: Sequelize.ENUM,
+        values: ['Pending', 'Shipped', 'Delivered'],
+        defaultValue: 'Pending',
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Orders');
+  }
+};
