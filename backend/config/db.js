d6ed666342cd0b6b699631d57e07c1b53ef6e732
ChanwooBrianKim <151323407+ChanwooBrianKim@users.noreply.github.com>
2024-08-28 23:50:14 +1000

    db.js -> .ts

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from 'sequelize';
-import configFile from './config.json' assert { type: 'json' };
-
-const env = process.env.NODE_ENV || 'development';
-const config = configFile[env];
-
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
-
-export default sequelize;
