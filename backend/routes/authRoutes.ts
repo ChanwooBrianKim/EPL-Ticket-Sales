22fb682dd70a9deb9e17b06eae97314a377d7566
ChanwooBrianKim <151323407+ChanwooBrianKim@users.noreply.github.com>
2024-08-29 23:32:17 +1000

    .js -> .ts

---
+++ backend/routes/authRoutes.ts
@@ -0,0 +1,10 @@
+import express from 'express'; // Importing Express.js
+import { register, login } from '../controllers/authController'; // Importing the register and login functions from the authController.ts file
+
+// Creating a new router
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+
+export default router;
---
+++ backend/routes/productRoutes.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from 'express';
+import { createProduct, getProducts, updateProduct, deleteProduct } from '../controllers/productController';
+
+const router = express.Router(); 
+
+router.post('/', (req: Request, res: Response) => createProduct(req, res)); 
+router.get('/', (req: Request, res: Response) => getProducts(req, res)); 
+router.put('/:id', (req: Request, res: Response) => updateProduct(req, res)); 
+router.delete('/:id', (req: Request, res: Response) => deleteProduct(req, res)); 
+
+export default router;
