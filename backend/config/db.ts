6f84fe974767ad995578b84e08c906ae2ff37803
ChanwooBrianKim <151323407+ChanwooBrianKim@users.noreply.github.com>
2024-08-29 23:29:18 +1000

    config error fixed

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -1,13 +1,23 @@
-import { Sequelize } from 'sequelize';
-import configFile from './config.json';
+import { Sequelize, Dialect } from 'sequelize';
+import configFile from './config.json' assert { type: 'json' };
 
 const env = process.env.NODE_ENV || 'development';
-const config = configFile[env] as {
-  database: string;
+
+// Define the type for the config object
+interface Config {
   username: string;
   password: string;
+  database: string;
+  host: string;
+  dialect: Dialect;
   [key: string]: any;
-};
+}
+
+interface ConfigFile {
+  [key: string]: Config;
+}
+
+const config: Config = (configFile as ConfigFile)[env];
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
