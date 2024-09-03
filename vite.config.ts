ea4f983b2ae2c5334536c613a7430de4d9814336
ChanwooBrianKim <151323407+ChanwooBrianKim@users.noreply.github.com>
2024-09-03 22:49:22 +1000

    complier

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist/src',
+    outDir: 'dist/src',                 // Output directory for Vite build
     rollupOptions: {
-      input: 'index.html',
+      input: 'index.html',              // Entry point
     },
   },
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:5000', // Proxy API requests to backend server
         changeOrigin: true,
         secure: false,
       },
