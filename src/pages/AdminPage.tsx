9189268a5eee76b88cbff1e4e8b29ec36637d77e
ChanwooBrianKim <brian980729@gmail.com>
2024-09-06 17:02:58 +1000

    admin page

---
+++ src/pages/AdminPage.tsx
@@ -0,0 +1,10 @@
+import { ProductUploadForm } from '../components/ProductUploadForm';
+
+export function AdminPage() {
+  return (
+    <div>
+      <h1>Admin Product Upload</h1>
+      <ProductUploadForm />
+    </div>
+  );
+}
