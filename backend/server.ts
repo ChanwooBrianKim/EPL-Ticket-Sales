734e9939bb200a8ec836cb6d40b4591b74686467
ChanwooBrianKim <151323407+ChanwooBrianKim@users.noreply.github.com>
2024-09-07 23:29:16 +1000

    userRoutes added

--- backend/server.ts
+++ backend/server.ts
@@ -4,21 +4,22 @@ dotenv.config();
 import express, { Application } from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import authRoutes from './routes/authRoutes.js'; // Import the authRoutes
-import productRoutes from './routes/productRoutes.js'; // Import the productRoutes
-import orderRoutes from './routes/orderRoutes.js'; // Import the orderRoutes
+import authRoutes from './routes/authRoutes';
+import productRoutes from './routes/productRoutes';
+import orderRoutes from './routes/orderRoutes';
+import userRoutes from './routes/userRoutes'; // Import the user routes
 
-// Initialize Express app
 const app: Application = express();
 
 // Middleware setup
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/api/auth', authRoutes); // Use authRoutes for /api/auth path
-app.use('/api/products', productRoutes); // Use productRoutes for /api/products path
-app.use('/api/orders', orderRoutes); // Use orderRoutes for /api/orders path
+// Route handlers
+app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/users', userRoutes); // Add the user routes
 
-// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
