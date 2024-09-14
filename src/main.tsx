7efc042c795523e2c988342f46be504e3349d086
ChanwooBrianKim <151323407+ChanwooBrianKim@users.noreply.github.com>
2024-09-15 00:08:06 +1000

    App.tsx -> extension removed

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react' // React is a default export from the react package
 import ReactDOM from 'react-dom/client' // ReactDOM is a named export from the react-dom package
-import App from './App.tsx' // Import the App component from the App.tsx file
+import App from './App' // Import the App component from the App.tsx file
 import "bootstrap/dist/css/bootstrap.min.css" // Import the Bootstrap CSS file
 import { BrowserRouter } from "react-router-dom" // Import the BrowserRouter component from the react-router-dom package
 
