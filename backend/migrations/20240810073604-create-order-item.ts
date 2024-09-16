9243d1d0a0c3275651f0e5859cfec9e1e2074f41
ChanwooBrianKim <151323407+ChanwooBrianKim@users.noreply.github.com>
2024-09-16 23:35:21 +1000

    create order item migration updated

--- backend/migrations/20240810073604-create-order-item.ts
+++ backend/migrations/20240810073604-create-order-item.ts
@@ -8,11 +8,31 @@ export async function up(queryInterface: QueryInterface) {
       primaryKey: true,
       type: DataTypes.INTEGER,
     },
+    orderId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Orders', // Relates to Orders table
+        key: 'id',
+      },
+      onDelete: 'CASCADE', // Delete order items if the order is deleted
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Products', // Relates to Products table
+        key: 'id',
+      },
+      onDelete: 'SET NULL', // Set product to NULL if it's deleted, but keep order item record
+    },
     quantity: {
       type: DataTypes.INTEGER,
+      allowNull: false,
     },
     priceAtOrder: {
       type: DataTypes.FLOAT,
+      allowNull: false,
     },
     createdAt: {
       allowNull: false,
