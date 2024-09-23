c63783566d80c9966a596701bd95b0c5a9e411e1
ChanwooBrianKim <151323407+ChanwooBrianKim@users.noreply.github.com>
2024-09-23 22:21:51 +1000

    import order -> updateOrderStatus corrected

--- backend/routes/orderRoutes.ts
+++ backend/routes/orderRoutes.ts
@@ -1,11 +1,11 @@
 import express, { Request, Response } from 'express'; 
-import { createOrder, getOrders, updateOrder, deleteOrder } from '../controllers/orderController.js';
+import { createOrder, getOrders, updateOrderStatus, deleteOrder } from '../controllers/orderController.js';
 
 const router = express.Router();
 
 router.post('/', (req: Request, res: Response) => createOrder(req, res)); 
 router.get('/', (req: Request, res: Response) => getOrders(req, res)); 
-router.put('/:id', (req: Request, res: Response) => updateOrder(req, res)); 
+router.put('/:id', (req: Request, res: Response) => updateOrderStatus(req, res)); 
 router.delete('/:id', (req: Request, res: Response) => deleteOrder(req, res)); 
 
 export default router;
